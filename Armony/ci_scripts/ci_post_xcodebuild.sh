1ab532de89950734e541db00da5456b2f8bdc7a6
yildizkoray <koray.yildizz@gmail.com>
2026-03-23 23:11:15 +0300

    Revert "fix: address ci_scripts review findings"
    This reverts commit 12d63dbeaa56903a15c9224d17dc00ac777c2146.

--- Armony/ci_scripts/ci_post_xcodebuild.sh
+++ Armony/ci_scripts/ci_post_xcodebuild.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 if [[ -n $CI_ARCHIVE_PATH ]]; then
 
     UPLOAD_SYMBOLS_PATH="$CI_DERIVED_DATA_PATH/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols"
-
-    if [ ! -f "$UPLOAD_SYMBOLS_PATH" ]; then
-        echo "Error: upload-symbols script not found at $UPLOAD_SYMBOLS_PATH" >&2
-        exit 1
-    fi
-
     INFO_PLIST_PATH=""
 
     if [ "$CI_XCODE_SCHEME" == "Armony" ]; then
@@ -18,7 +12,7 @@ if [[ -n $CI_ARCHIVE_PATH ]]; then
         INFO_PLIST_PATH='../Armony/Resources/Firebase/GoogleService-Info-Debug.plist'
     fi
 
-"$UPLOAD_SYMBOLS_PATH" -gsp "$INFO_PLIST_PATH" -p ios "$CI_ARCHIVE_PATH/dSYMs"
+$UPLOAD_SYMBOLS_PATH -gsp $INFO_PLIST_PATH -p ios $CI_ARCHIVE_PATH/dSYMs
 
 else
     echo "Archive path isn't available. Unable to run dSYMs uploading script."
